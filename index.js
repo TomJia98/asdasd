9b3643c677894d17f9c23278af07a2152352ddcb
tomjia98 <tomchristian1998@gmail.com>
2022-07-07 13:49:34 +0800

    fixed it going off screen as much

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const frown = document.getElementById("thing");
 setInterval(function () {
   frown.style.postition = "absolute";
 
-  frown.style.top = `${Math.floor(Math.random() * 100)}%`;
-  frown.style.left = `${Math.floor(Math.random() * 100)}%`;
-  frown.style.fontSize = `${Math.floor(Math.random() * 10)}em`;
-}, 1000);
+  frown.style.top = `${Math.floor(Math.random() * 90)}%`;
+  frown.style.left = `${Math.floor(Math.random() * 90)}%`;
+  frown.style.fontSize = `${Math.random() * 20}em`;
+}, 500);
